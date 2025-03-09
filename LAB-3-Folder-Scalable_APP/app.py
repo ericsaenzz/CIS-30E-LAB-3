85e32c07a135bd979ef4c2a34f4d8e22bdd3664a
Eric Chang <126522698+ericsaenzz@users.noreply.github.com>
2025-03-09 01:48:47 +0000

    Simple Flask with home and about routes

--- LAB-3-Folder-Scalable_APP/app.py
+++ LAB-3-Folder-Scalable_APP/app.py
@@ -1,1 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return  render_template('home.html')
+
+@app.route('/about')
+def about():
+    return "This is a simple scalable Flask app.The focus will be on setting up a scalable project structure with Python, creating reusable functions, and utilizing Flask for a basic web application setup."
+if __name__ == '__main__':
+    app.run(debug=True)
